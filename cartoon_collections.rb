4b750060e5776a83a740b6d10463b91816d455fb
Chris Roberts <chrisrobertspdx@gmail.com>
2017-10-24 21:36:40 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,8 @@ def summon_captain_planet(arr)# code an argument here
   }# Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)# code an argument here
+  return arr.collect{|e| e.size > 4}.size > 0# Your code here
 end
 
 def find_the_cheese# code an argument here
